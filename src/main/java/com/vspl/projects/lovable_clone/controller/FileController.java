32c2a16d442fa1af79614037ba27bfa4579d6361
sameersuryavanshi <suryavanshisameer79@gmail.com>
2026-04-16 15:03:17 -0400

    first commit

--- src/main/java/com/vspl/projects/lovable_clone/controller/FileController.java
+++ src/main/java/com/vspl/projects/lovable_clone/controller/FileController.java
@@ -4,6 +4,8 @@ import com.vspl.projects.lovable_clone.dto.project.FileTreeResponse;
 import com.vspl.projects.lovable_clone.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +16,9 @@ public class FileController {
 
     private static FileService fileService;
 
-    public ResponseEntity<FileTreeResponse>
+    @GetMapping
+    public ResponseEntity<FileTreeResponse> getFileTree(@PathVariable Long projectId){
+        Long userId = 1L;
+        return ResponseEntity.ok(fileService.getFileTree(projectId, userId));
+    }
 }
--- src/main/java/com/vspl/projects/lovable_clone/service/FileService.java
+++ src/main/java/com/vspl/projects/lovable_clone/service/FileService.java
@@ -1,4 +1,9 @@
 package com.vspl.projects.lovable_clone.service;
 
+import com.vspl.projects.lovable_clone.dto.project.FileTreeResponse;
+import org.jspecify.annotations.Nullable;
+
 public interface FileService {
+
+    FileTreeResponse getFileTree(Long projectId, Long userId);
 }
